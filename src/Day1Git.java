4933be00af4ea22826e4b3c0336b2017fd59dddc
Muhammed KAPLAN <avmuhammedkaplan@gmail.com>
2022-08-17 18:23:04 +0300

    2.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,6 +1,7 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Hello git 1. deneme");
+        System.out.println("Hello git 2. deneme");
 
     }
 }
